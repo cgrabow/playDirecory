e8c60a71d574c021521b99b80352d67279afce47
Christine Grabow <christine.grabow@tradeshift.com>
2023-02-13 13:28:03 +0100

    Initial setup of project
    Add `Hello World` to check that the setup is working.

---
+++ src/test/java/com/example/playerproject/PlayerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
