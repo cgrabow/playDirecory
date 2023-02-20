26811d954bdd59a93dc448cde548eee65fb5a072
Christine Grabow <christine-m-g@hotmail.com>
2023-02-20 09:29:32 +0100

    Database Setup
    Add necessary dependencies and plugins to the `pom.xml` to set up an in-memory database.
    Define database properties in the `application.properties`.
    Create tables and input to the tables when starting the application.

--- src/main/resources/db/migration/V1__create_player_level_table.sql
+++ src/main/resources/db/migration/V1__create_player_level_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE players (
-                         id   INTEGER       NOT NULL AUTO_INCREMENT,
+                         id   INTEGER       NOT NULL UNIQUE,
                          name TEXT          NOT NULL,
                          geography char(3)  NOT NULL,
                          PRIMARY KEY (id)
 );
 
 CREATE TABLE playerLevels (
-                        id   INTEGER        NOT NULL AUTO_INCREMENT,
+                        id   INTEGER        NOT NULL,
                         playerId INTEGER    NOT NULL,
                         level TEXT          NOT NULL,
                         game TEXT           NOT NULL,
---
+++ src/main/resources/db/migration/V2__add_data_to_db_at_startup.sql
@@ -0,0 +1,13 @@
+INSERT INTO players VALUES (1, 'Alexander', 'DKK');
+INSERT INTO players VALUES (2, 'Andreas', 'DKK');
+INSERT INTO players VALUES (3, 'Thomas', 'MVD');
+INSERT INTO players VALUES (4, 'Michael', 'SVN');
+INSERT INTO players VALUES (5, 'Lise', 'DEU');
+INSERT INTO players VALUES (6, 'Simone', 'DEU');
+
+INSERT INTO playerLevels VALUES (1, 4, 'N00B', 'GOLF');
+INSERT INTO playerLevels VALUES (2, 3, 'INVINCIBLE', 'TENNIS');
+INSERT INTO playerLevels VALUES (3, 4, 'PRO', 'BASEBALL');
+INSERT INTO playerLevels VALUES (4, 1, 'INVINCIBLE', 'BOWLING');
+INSERT INTO playerLevels VALUES (5, 2, 'N00B', 'BOWLING');
+INSERT INTO playerLevels VALUES (6, 5, 'INVINCIBLE', 'GOLF');
